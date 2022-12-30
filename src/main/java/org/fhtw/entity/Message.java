b34c6e892b0b05f9aa12a378e75dfbe739343e09
Ahmad Schurbaji <schurbaji.ahmad@gmail.com>
2022-12-30 11:08:48 +0100

    changed attributes names

--- src/main/java/org/fhtw/entity/Message.java
+++ src/main/java/org/fhtw/entity/Message.java
@@ -3,58 +3,58 @@ package org.fhtw.entity;
 public class Message {
     //12331;ahmad;update;11-04-2015 22:01:33:023;11-04-2015 05:07:20:02
     //          "EmployeeID", "Name", "Task", "Date-from", "Date-to"
-    private int EmployeeId;
-    private String EmployeeName;
-    private String Task;
-    private String DateFrom;
-    private String DateTo;
+    private int employeeId;
+    private String employeeName;
+    private String task;
+    private String dateFrom;
+    private String dateTo;
 
     public Message(int employeeId, String employeeName, String task, String dateFrom, String dateTo) {
-        this.EmployeeId = employeeId;
-        this.EmployeeName = employeeName;
-        this.Task = task;
-        this.DateFrom = dateFrom;
-        this.DateTo = dateTo;
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.task = task;
+        this.dateFrom = dateFrom;
+        this.dateTo = dateTo;
     }
 
     public int getEmployeeId() {
-        return EmployeeId;
+        return employeeId;
     }
 
     public void setEmployeeId(int employeeId) {
-        EmployeeId = employeeId;
+        this.employeeId = employeeId;
     }
 
     public String getEmployeeName() {
-        return EmployeeName;
+        return employeeName;
     }
 
     public void setEmployeeName(String employeeName) {
-        EmployeeName = employeeName;
+        this.employeeName = employeeName;
     }
 
     public String getTask() {
-        return Task;
+        return task;
     }
 
     public void setTask(String task) {
-        Task = task;
+        this.task = task;
     }
 
     public String getDateFrom() {
-        return DateFrom;
+        return dateFrom;
     }
 
     public void setDateFrom(String dateFrom) {
-        DateFrom = dateFrom;
+        this.dateFrom = dateFrom;
     }
 
     public String getDateTo() {
-        return DateTo;
+        return dateTo;
     }
 
     public void setDateTo(String dateTo) {
-        DateTo = dateTo;
+        this.dateTo = dateTo;
     }
     public String[]  getAllData(){
         return new String[]{String.valueOf(getEmployeeId()),getEmployeeName(),getTask(),getDateFrom(),getDateTo() };
